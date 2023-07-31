621632b81878465695784ff874cbdf66d9d4bf8b
GustavoMarquesMartins <gustavomarquesmartins11@gmail.com>
2023-07-31 19:49:47 -0300

    Projeto finalizado

--- src/main/java/med/voll/api/infra/Exception/TratamentoException.java
+++ src/main/java/med/voll/api/infra/Exception/TratamentoException.java
@@ -1,30 +1,65 @@
 package med.voll.api.infra.Exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestControllerAdvice
 public class TratamentoException {
+    private record TratamentoExceptionResponseDados(String campos, String mensagem) {
+        public TratamentoExceptionResponseDados(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity error404() {
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity error400(MethodArgumentNotValidException error) {
-        var listaError = error.getFieldErrors();
-        return ResponseEntity.badRequest().body(listaError.stream().map(TratamentoExceptionResponseDados::new).toList());
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
-    private record TratamentoExceptionResponseDados(String campos, String mensagem) {
-        public TratamentoExceptionResponseDados(FieldError error) {
-            this(error.getField(), error.getDefaultMessage());
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
         }
     }
 }
